47217be39bb5e8e0c40deb9c241183d2710b3093
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-06-08 22:04:31 +0200

    Added dropdownMenu for character icons

--- src/interface.js
+++ src/interface.js
@@ -1,1 +1,11 @@
 console.log("interface.js says: this seem to be working");
+
+const dropdownButton = document.getElementById("dropdownButton");
+const dropdownMenu = document.getElementById("dropdownMenu");
+
+dropdownButton.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("show");
+  dropdownButton.textContent = dropdownMenu.classList.contains("show")
+    ? "Hide Characters"
+    : "Display Characters";
+});
