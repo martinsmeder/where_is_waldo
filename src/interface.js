5add3f7de468bb6c3db96ae598f9850ad0c7aa8c
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-06-09 22:08:16 +0200

    Added createCircle()

--- src/interface.js
+++ src/interface.js
@@ -1,13 +1,37 @@
-// 4. Footer
-
 console.log("interface.js says: this seem to be working");
 
-const dropdownButton = document.getElementById("dropdownButton");
-const dropdownMenu = document.getElementById("dropdownMenu");
+const Controller = (() => {
+  const content = document.getElementById("content");
+  const dropdownButton = document.getElementById("dropdownButton");
+  const dropdownMenu = document.getElementById("dropdownMenu");
 
-dropdownButton.addEventListener("click", () => {
-  dropdownMenu.classList.toggle("show");
-  dropdownButton.textContent = dropdownMenu.classList.contains("show")
-    ? "▲ Hide Characters ▲"
-    : "▼ Show Characters ▼";
-});
+  const createCircle = (x, y) => {
+    const circle = document.createElement("div");
+    circle.className = "circle";
+    circle.style.left = `${x}px`;
+    circle.style.top = `${y}px`;
+    content.appendChild(circle);
+  };
+
+  const init = () => {
+    content.addEventListener("click", (event) => {
+      const x = event.clientX - content.offsetLeft;
+      const y = event.clientY - content.offsetTop;
+      console.log("Click registered");
+      createCircle(x, y);
+    });
+
+    dropdownButton.addEventListener("click", () => {
+      dropdownMenu.classList.toggle("show");
+      dropdownButton.textContent = dropdownMenu.classList.contains("show")
+        ? "▲ Hide Characters ▲"
+        : "▼ Show Characters ▼";
+    });
+  };
+
+  return {
+    init,
+  };
+})();
+
+Controller.init();
