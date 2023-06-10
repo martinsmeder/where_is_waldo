bc84ea26487c02cf4adcbea306beb9a3761ec7f3
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-06-10 10:20:09 +0200

    Added removeCircle()

--- src/interface.js
+++ src/interface.js
@@ -4,21 +4,48 @@ const Controller = (() => {
   const content = document.getElementById("content");
   const dropdownButton = document.getElementById("dropdownButton");
   const dropdownMenu = document.getElementById("dropdownMenu");
+  const backgroundImg = document.getElementById("backgroundImg");
+  const header = document.querySelector("header");
+  const circles = [];
 
   const createCircle = (x, y) => {
+    console.log("Clicked position: ", x, y);
+    console.log("Header height: ", header.offsetHeight);
+
+    // Check if the click event occurred within the header
+    if (y < header.offsetHeight) {
+      console.log("Clicked within the header. Circle not added.");
+      return;
+    }
+
     const circle = document.createElement("div");
     circle.className = "circle";
-    circle.style.left = `${x}px`;
-    circle.style.top = `${y}px`;
+    circle.style.left = `${x - 25}px`; // Adjusted x position
+    circle.style.top = `${y - 25}px`; // Adjusted y position
     content.appendChild(circle);
+    circles.push(circle);
+  };
+  const removeCircle = () => {
+    if (circles.length > 0) {
+      const circleToRemove = circles.pop();
+      content.removeChild(circleToRemove);
+    }
   };
 
   const init = () => {
+    let isAddingCircle = true;
+
     content.addEventListener("click", (event) => {
-      const x = event.clientX - content.offsetLeft;
-      const y = event.clientY - content.offsetTop;
-      console.log("Click registered");
-      createCircle(x, y);
+      if (isAddingCircle) {
+        const rect = backgroundImg.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        createCircle(x, y);
+      } else {
+        removeCircle();
+      }
+
+      isAddingCircle = !isAddingCircle;
     });
 
     dropdownButton.addEventListener("click", () => {
